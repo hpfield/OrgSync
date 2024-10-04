1b86cc8b6f7236d266dd3055b0ef12199f1b2cbb
dec2g14 <dec2g14@gmail.com>
2024-10-04 16:05:26 +0100

    Added additional_fields_gtr.ipynb

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-"""
-Establish the project root filepath, wrangle the raw data files and setup any other initial params
-"""
-
 import json
 import os
 import yaml
@@ -19,23 +15,25 @@ file_paths = [
 ]
 
 # Initialize an empty list to store combined data
-combined_data = []
+uk_data = []
 
-# Iterate over the file paths, read and append data to combined_data
+# Iterate over the file paths, read, filter and append data to uk_data
 for file_path in file_paths:
     full_path = os.path.join(input_path, file_path)
     with open(full_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
-        combined_data.extend(data)
+        # Filter the data where country is "UK"
+        uk_entries = [entry for entry in data if entry.get("country") == "UK"]
+        uk_data.extend(uk_entries)
 
 # Specify the output file path
-output_file_path = os.path.join(script_directory, "data", "raw", "combined_organizations.json")
+output_file_path = os.path.join(script_directory, "data", "raw", "uk_data.json")
 
-# Write the combined data to the output file
+# Write the filtered UK data to the output file
 with open(output_file_path, 'w', encoding='utf-8') as output_file:
-    json.dump(combined_data, output_file, indent=4)
+    json.dump(uk_data, output_file, indent=4)
 
-print(f"Combined data has been written to {output_file_path}")
+print(f"Filtered UK data has been written to {output_file_path}")
 
 # Path to the config file
 config_file_path = os.path.join(script_directory, "cfg/config.yaml")
@@ -59,4 +57,3 @@ with open(config_file_path, 'w', encoding='utf-8') as config_file:
     yaml.safe_dump(config_data, config_file)
 
 print(f"Project root has been set to {project_root_value} in {config_file_path}")
-
