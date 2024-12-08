de4fff979c21938d7383dfa68eb7e78d738a9539
dec2g14 <dec2g14@gmail.com>
2024-12-08 15:15:00 +0000

    Updates to setup.py & util_funcs.py for simplified data processing

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ import os
 import yaml
 import argparse
 from src.util_funcs import (
-    SchemaProcessor, 
-    normalize_json_fields, 
-    read_json, 
+    process_gtr_data,
+    load_json,
     save_json,
     add_const_field_json,
     remove_fields,
@@ -13,132 +12,137 @@ from src.util_funcs import (
     convert_entries_to_str
 )
 
-# Allow testing only on Cordis data only
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--all_data", 
-    default = True, 
-    type = bool, 
-    help="Create separate json for GtR data")
+def main():
+    # Set up argument parser to allow testing only on Cordis data only
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--all_data",
+        default=True,
+        type=bool,
+        help="Process both Cordis and GTR data vs. Cordis only"
+    )
 
-# Determine the directory where this script is located
-script_directory = os.path.dirname(os.path.abspath(__file__))
+    # Determine the directory where this script is located
+    script_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Define the base path and the file paths to combine
-input_path = os.path.join(script_directory, "data/raw/all_scraped/")
-file_paths = [
-    "cordis/2024_07/FP7/organization.json",
-    "cordis/2024_07/Horizon 2020/organization.json",
-    "cordis/2024_07/Horizon Europe/organization.json",
-]
+    # Define the base path for raw data
+    input_path = os.path.join(script_directory, "data/raw/all_scraped/")
+    # Define the file paths to combine for Cordis data
+    cordis_files = [
+        "cordis/2024_07/FP7/organization.json",
+        "cordis/2024_07/Horizon 2020/organization.json",
+        "cordis/2024_07/Horizon Europe/organization.json",
+    ]
+    gtr_file = "gtr/scraped/2024_07/organisations.json"
 
-# Initialize an empty list to store combined data
-uk_data = []
+    # Combine and filter Cordis data
+    uk_data = []
 
-# Iterate over the file paths, read, filter and append data to uk_data
-for file_path in file_paths:
-    full_path = os.path.join(input_path, file_path)
-    with open(full_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-        # Filter the data where country is "UK"
+    # Iterate over the file paths, read, filter and append data to uk_data
+    for file_path in cordis_files:
+        full_path = os.path.join(input_path, file_path)
+        # with open(full_path, 'r', encoding='utf-8') as file:
+        #     data = json.load(file)
+        #     # Filter the data where country is "UK". Unlike GtR, Cordis has non-UK entries
+        #     uk_entries = [entry for entry in data if entry.get("country") == "UK"]
+        #     uk_data.extend(uk_entries)
+        data = load_json(full_path)
         uk_entries = [entry for entry in data if entry.get("country") == "UK"]
         uk_data.extend(uk_entries)
 
-# Specify the output file path
-output_file_path = os.path.join(script_directory, "data", "raw", "uk_data.json")
+    # Specify the output file path
+    output_file_path = os.path.join(script_directory, "data", "raw", "uk_data.json")
 
-# Write the filtered UK data to the output file
-with open(output_file_path, 'w', encoding='utf-8') as output_file:
-    json.dump(uk_data, output_file, indent=4)
+    # Write the filtered UK data to the output file
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        json.dump(uk_data, output_file, indent=4)
+    print(f"Filtered Cordis data has been written to {output_file_path}")
 
-print(f"Filtered Cordis data has been written to {output_file_path}")
+    if parser.parse_args().all_data:
+        # Save initial Cordis data with added dataset field
+        cordis_data = add_const_field_json(uk_data, "dataset", "cordis")
+        save_json(
+            cordis_data, 
+            'cordis_data.json', 
+            save_dir=os.path.join(script_directory, 'data/raw/')
+        )
+        # load and apply processing function for gtr data to transform to flat structure
+        gtr_file = os.path.join(input_path, gtr_file)
+        raw_gtr_data = load_json(gtr_file)
+        processed_gtr = process_gtr_data(raw_gtr_data)
+        save_json(
+            processed_gtr, 
+            'gtr_data.json', 
+            save_dir=os.path.join(script_directory, 'data/raw/')
+        )
 
+        # Any fields not named in fields to keep will be excluded from final uk_data.json
+        gtr_fields_to_keep = [
+            "dataset",
+            "name",
+            "id",
+            "address.postCode",
+        ]
+        cordis_fields_to_keep = [
+            "dataset",
+            "name",
+            "shortName",
+            "organisationID",
+            "postCode",
+        ]
+        # Remap names as postcode and unique id have different names in gtr and cordis
+        map_names_gtr = {
+            "id": "unique_id",
+            "address.postCode": "postcode"
+        }
+        
+        map_names_cordis = {
+            "organisationID": "unique_id",
+            "postCode": "postcode"
+        }
 
-if parser.parse_args().all_data:
-    # process gtr data
-    cordis_data = add_const_field_json(uk_data, "dataset", "cordis")
-    save_json(cordis_data, os.path.join(script_directory, 'data/raw/cordis_data.json'))
-    print(f"Filtered Cordis data has been written to {os.path.join(script_directory, 'data/raw/cordis_data.json')}")
-    
-    base_dir = os.path.join("data", "raw", "all_scraped")
-    file_path = os.path.join(base_dir,"gtr", "scraped", "2024_07", "organisations.json")
-    schema_path = os.path.join(base_dir, "gtr", "scraped", "schemas", "organisation.json")
+        # Add dataset field to processed GTR data
+        gtr_data = add_const_field_json(processed_gtr, "dataset", "gtr")
+        
+        # Apply transformations to both datasets
+        gtr_data = remove_fields(gtr_data, gtr_fields_to_keep)
+        gtr_data = map_names_json(gtr_data, map_names_gtr)
 
-    raw_data = read_json(file_path)
-    schema = read_json(schema_path)
-    processor = SchemaProcessor(raw_data, schema)
-    processed_data = [processor.process_data(item, "organisations") for item in raw_data]
-    normalised_data = normalize_json_fields(processed_data)
-    gtr_data = add_const_field_json(normalised_data, "dataset", "gtr")
-    save_dir = os.path.join(script_directory, "data/raw/")
-    save_json(gtr_data, os.path.join(save_dir, 'gtr_data.json'))
-    print(f"Normalised GtR data has been written to {os.path.join(save_dir, 'gtr_data.json')}")
+        cordis_data = remove_fields(cordis_data, cordis_fields_to_keep)
+        cordis_data = map_names_json(cordis_data, map_names_cordis)
+        
+        # Combine and save final dataset
+        uk_data = cordis_data + gtr_data
+        # some unique ids and postcodes may be integers, convert to strings
+        convert_entries_to_str(uk_data, ["postcode", "unique_id"])
+        save_json(
+            uk_data, 
+            'uk_data.json', 
+            save_dir=os.path.join(script_directory, 'data/raw/')
+        )
 
-    # combine datasets including only fields of interest
-    gtr_fields_to_keep = [
-        "dataset",
-        "name",
-        "id",
-        # "address.type",
-        "address.postCode",
-        # "address.region",
-    ]
+    # Path to the config file
+    config_file_path = os.path.join(script_directory, "cfg/config.yaml")
 
-    cordis_fields_to_keep = [
-        "dataset",
-        "name",
-        "shortName",
-        # "projectAcronym",
-        "organisationID",
-        # "city", 
-        # "geolocation",
-        "postCode",
-        # "street",
-        # "nutsCode",
-        # "rcn",
-        # "organisationURL",
-    ]
+    # Append the project root to the config file
+    project_root_key = "project_root"
+    project_root_value = script_directory
 
-    map_names_gtr = {
-        "id": "unique_id",
-        "address.postCode": "postcode"
-    }
-    map_names_cordis = {
-        "organisationID": "unique_id",
-        "postCode": "postcode"
-    }
+    # Load existing config if it exists, or create a new dict
+    if os.path.exists(config_file_path):
+        with open(config_file_path, 'r', encoding='utf-8') as config_file:
+            config_data = yaml.safe_load(config_file) or {}
+    else:
+        config_data = {}
 
-    gtr_data = remove_fields(gtr_data, gtr_fields_to_keep)
-    gtr_data = map_names_json(gtr_data, map_names_gtr)
+    # Update the config with the project root path
+    config_data[project_root_key] = project_root_value
 
-    cordis_data = remove_fields(cordis_data, cordis_fields_to_keep)
-    cordis_data = map_names_json(cordis_data, map_names_cordis)
-    
-    uk_data = cordis_data + gtr_data
-    convert_entries_to_str(uk_data, ["postcode", "unique_id"])
-    save_json(uk_data, os.path.join(script_directory, 'data/raw/uk_data.json'))
+    # Write the updated config back to the config file
+    with open(config_file_path, 'w', encoding='utf-8') as config_file:
+        yaml.safe_dump(config_data, config_file)
+        
+    print(f"Project root has been set to {project_root_value} in {config_file_path}")
 
-
-
-# Path to the config file
-config_file_path = os.path.join(script_directory, "cfg/config.yaml")
-
-# Append the project root to the config file
-project_root_key = "project_root"
-project_root_value = script_directory
-
-# Load existing config if it exists, or create a new dict
-if os.path.exists(config_file_path):
-    with open(config_file_path, 'r', encoding='utf-8') as config_file:
-        config_data = yaml.safe_load(config_file) or {}
-else:
-    config_data = {}
-
-# Update the config with the project root path
-config_data[project_root_key] = project_root_value
-
-# Write the updated config back to the config file
-with open(config_file_path, 'w', encoding='utf-8') as config_file:
-    yaml.safe_dump(config_data, config_file)
-
-print(f"Project root has been set to {project_root_value} in {config_file_path}")
+if __name__ == "__main__":
+    main()
